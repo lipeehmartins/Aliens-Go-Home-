3c20c1eacef48d13084d10f31732d4a28ed7ed8f
lipeehmartins <lipihmartins@gmail.com>
2021-04-06 15:16:46 -0400

    Created Ground and Sky jsx, plus utils to export global constants

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Sky from './Sky';
+import Ground from './Ground';
 
 const Canvas = () => {
   const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
@@ -8,7 +10,9 @@ const Canvas = () => {
       preserveAspectRatio="xMaxYMax none"
       viewBox={viewBox}
     >
-      <circle cx={0} cy={0} r={50} />
+        <Sky />
+        <Ground />
+        <circle cx={0} cy={0} r={50} />
     </svg>
   );
 };
